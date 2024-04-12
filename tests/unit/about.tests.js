50fd6a2fbbcbd2883082b50e2c33811f3a94bac7
St4n1l <stanil.h@abv.bg>
2024-04-12 19:35:06 +0300

    fixed unit tests

--- tests/unit/about.tests.js
+++ tests/unit/about.tests.js
@@ -3,9 +3,9 @@ const fetch = require('node-fetch');
 
 suite('About page', function() {
   test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/about");
+    let res = await fetch("http://localhost:8080/about");
     let body = await res.text();
-    assert.ok(body.includes("<title>About</title>"));
     assert.ok(body.includes("<h1>About</h1>"));
+    assert.ok(body.includes("<p>This is the Regular exam for Software Engineering and DevOps course @ SoftUni</p>"));
   });
 });
--- tests/unit/add-grades.tests.js
+++ tests/unit/add-grades.tests.js
@@ -3,13 +3,13 @@ const fetch = require('node-fetch');
 
 suite('Add Grades page', function() {
   test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/Add-Grade");
+    let res = await fetch("http://localhost:8080/Add-Grade");
     let body = await res.text();
     assert.ok(body.includes("<h1>Add New Grade</h1>"));
   });
 
   test('Grade HTML form', async function() {
-    let res = await fetch("http://localhost:8888/Add-Grade");
+    let res = await fetch("http://localhost:8080/Add-Grade");
     let body = await res.text();
     
     let subjectFieldFound = body.includes('<input id="subject" type="text" name="subject"/>');
@@ -24,7 +24,7 @@ suite('Add Grades page', function() {
 
   test('Add valid grade', async function() {
     let res = await fetch(
-      "http://localhost:8888/Add-Grade",
+      "http://localhost:8080/Add-Grade",
       {
         method: 'POST',
         headers: {
@@ -35,13 +35,13 @@ suite('Add Grades page', function() {
     );
     let body = await res.text();
     let gradesReturned = body.includes(
-		"<ul><li>English (4.50)</li><li>Math (5.50)</li><li>Programming Basics (6.00)</li><li>Physics (3.90)</li></ul>");
+		"<ul><li>English (5.50)</li><li>Math (4.50)</li><li>Programming Basics (6.00)</li><li>Physics (3.90)</li></ul>");
     assert.ok(gradesReturned, "Add grade failed");
   });
 
   test('Add invalid grade', async function() {
      let res = await fetch(
-      "http://localhost:8888/Add-Grade",
+      "http://localhost:8080/Add-Grade",
       {
         method: 'POST',
         headers: {
@@ -54,9 +54,9 @@ suite('Add Grades page', function() {
     let errMsg = body.includes("Cannot add grade. Subject and value fields are required!");
     assert.ok(errMsg, "Add invalid grade should display an error message");
 
-    res = await fetch("http://localhost:8888/");
+    res = await fetch("http://localhost:8080/");
     body = await res.text();
-	  assert.ok(body.includes("Grades: <b>3</b>"), 
+	  assert.ok(body.includes("Grades: <b>4</b>"), 
 		"Add invalid grade should not change the grades count");
   });
 });
